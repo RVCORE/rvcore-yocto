edb6f33c66260da5143e4b8ae08f0a89b9f7d958
Zhang Ze <zhangze@rvcore.com>
2022-11-15 17:59:58 +0800

    add license

--- meta-rvcore/qemu_run_fullsystem.sh
+++ meta-rvcore/qemu_run_fullsystem.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+#***************************************************************************************
+# Copyright (c) 2022 Beijing Vcore Technology Co.,Ltd.
+#
+# rvcore-yocto is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#
+# See the Mulan PSL v2 for more details.
+#*************************************************************************************
+
+
 MACHINE=qemuriscv64 runqemu nographic qemuparams="-smp 12 -m 16000"
