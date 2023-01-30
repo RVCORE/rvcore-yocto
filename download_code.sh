b78fd9002a625a4f81004d3c4dbffc9a7d001912
Zhang Ze <zhangze@rvcore.com>
2023-01-30 14:18:59 +0800

    fix download_code.sh

--- download_code.sh
+++ download_code.sh
@@ -20,14 +20,23 @@ git config --global http.sslVerify false
 
 #download riscv repo
 repo init -u https://github.com/riscv/meta-riscv  -b master -m tools/manifests/riscv-yocto.xml
-repo sync
+repo sync -d -c
 
-repo start work --rev d6e3efd54a3c1361fecf2a56c6f4f590fbe676d9 meta-riscv/
-repo start work --rev dd137feabdad2e7a2b3d107c703aad577a2e79c7 openembedded-core/
-repo start work --rev b986eac18b6a8bf633f5ef15f32f68de4c86173b openembedded-core/bitbake/
-repo start work --rev ca7082caa800124b4e0a7b36175f60387d1e3f8c meta-openembedded/
+BRANCH_NAME=20230130
+
+repo start ${BRANCH_NAME} --rev d6e3efd54a3c1361fecf2a56c6f4f590fbe676d9 meta-riscv/
+repo start ${BRANCH_NAME} --rev dd137feabdad2e7a2b3d107c703aad577a2e79c7 openembedded-core/
+repo start ${BRANCH_NAME} --rev b986eac18b6a8bf633f5ef15f32f68de4c86173b openembedded-core/bitbake/
+repo start ${BRANCH_NAME} --rev ca7082caa800124b4e0a7b36175f60387d1e3f8c meta-openembedded/
 
 #download clang layer
-git clone https://github.com/kraj/meta-clang.git
-cd meta-clang
-git checkout -b work 36d2b65f81542ea5e36664cfdc4d1b5763b9ba4b
+if [ -d meta-clang ]
+then
+	cd meta-clang
+	git fetch --all
+else
+	git clone https://github.com/kraj/meta-clang.git
+	cd meta-clang
+fi
+
+git checkout -b ${BRANCH_NAME} 36d2b65f81542ea5e36664cfdc4d1b5763b9ba4b
