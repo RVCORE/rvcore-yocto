bc1e96a8cf059ff6033bcd63588c0d56bcb44d2c
Zhang Ze <zhangze@rvcore.com>
2025-04-02 10:48:33 +0800

    update download.sh
    Signed-off-by: Zhang Ze <zhangze@rvcore.com>

--- download_code.sh
+++ download_code.sh
@@ -19,24 +19,68 @@ git config --global http.postBuffer 524288000
 git config --global http.sslVerify false
 
 #download riscv repo
-repo init -u https://github.com/riscv/meta-riscv  -b 9e7fbe14da6bc2655a47c866527abf45c9c73045 -m tools/manifests/riscv-yocto.xml
+repo init -u https://github.com/riscv/meta-riscv  -b 07658f1181e3ecd5f69b3e62c5319939426ffb02 -m tools/manifests/riscv-yocto.xml
 repo sync -d -c
 
-BRANCH_NAME=20230309
+BRANCH_NAME=20250401
 
-repo start ${BRANCH_NAME} --rev 880aa9b7e39502d196dcaf296cbf0efe0d585f50 meta-riscv/
-repo start ${BRANCH_NAME} --rev 3cf894b8a9c4fa14fcc7c7445e85e9ae3192b398 openembedded-core/
-repo start ${BRANCH_NAME} --rev e859ce61a7422425960342a47785468cef49caa1 openembedded-core/bitbake/
-repo start ${BRANCH_NAME} --rev eb0d072f9ecfa175c79b781fc62265914abeb94e meta-openembedded/
+repo start ${BRANCH_NAME} --rev 07658f1181e3ecd5f69b3e62c5319939426ffb02 meta-riscv/
+repo start ${BRANCH_NAME} --rev 8147a884c68d8fdd89939a8443a902b65297520c meta-openembedded/
+repo start ${BRANCH_NAME} --rev e894acce6ede8bedafc1859ea0345ee6d80e9c74 poky
 
-#download clang layer
+
+echo "download clang revival layer"
+if [ -d meta-clang-revival ]
+then
+	cd meta-clang-revival
+	git fetch --all
+else
+	git clone https://github.com/zboszor/meta-clang-revival
+	cd meta-clang-revival
+fi
+
+git checkout -b ${BRANCH_NAME} d54230b2763af3e549490576606ff54702a267ce
+
+cd ..
+
+echo "download clang layer"
 if [ -d meta-clang ]
 then
 	cd meta-clang
 	git fetch --all
 else
-	git clone https://github.com/kraj/meta-clang.git
+	git clone https://github.com/kraj/meta-clang
 	cd meta-clang
 fi
 
-git checkout -b ${BRANCH_NAME} 87d41f7dd7a69bbf15973506faf113a83bd60511
+git checkout -b ${BRANCH_NAME} 93dec321980091b8abfa1f7d72c50b844c63167c
+
+cd ..
+
+echo "download dpdk layer"
+if [ -d meta-dpdk ]
+then
+	cd meta-dpdk
+	git fetch --all
+else
+	git clone https://git.yoctoproject.org/meta-dpdk
+	cd meta-dpdk
+fi
+
+git checkout -b ${BRANCH_NAME} 4f5ff4c3c86c90d9a2da787740be6b8b8b4de821
+
+cd ..
+
+echo "download meta-virtualization layer"
+if [ -d meta-virtualization ]
+then
+	cd meta-virtualization
+	git fetch --all
+else
+	git clone https://git.yoctoproject.org/meta-virtualization
+	cd meta-virtualization
+fi
+
+git checkout -b ${BRANCH_NAME} a309b738e73860781ee9099de5fc9df7ee22950c
+
+cd ..
